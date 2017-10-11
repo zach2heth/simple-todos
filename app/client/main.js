95098f7faad1c3cbc3bf3dd5156430e1a4823aa6
zach2heth <zach2heth@gmail.com>
2017-10-10 20:04:45 -1000

    Task 4: Collections, Temporary UI State, Adding user accounts

--- app/client/main.js
+++ app/client/main.js
@@ -1,3 +1,6 @@
+
 import '../imports/ui/body.js';
 
 import '../imports/api/tasks.js';
+
+import '../imports/startup/accounts-config.js';
--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -1,14 +1,31 @@
 import { Template } from 'meteor/templating';
 
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks.js';
+
+import { Meteor } from 'meteor/meteor';
 
 import './body.html';
 
 import './task.js';
 
+
+Template.body.onCreated(function bodyOnCreated () {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     return Tasks.find({}, {sort: { createdAt: -1} });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
   },
 });
 
@@ -24,9 +41,15 @@ Template.body.events({
     Tasks.insert({
       text,
       createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
     });
 
     target.text.value = '';
+    'change .hide-completed input' (event, instance)
+    {
+      instance.state.set('hideCompleted', event.target.checked);
+    }
   },
 
 
