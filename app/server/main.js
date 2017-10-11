ae1fff54391d4883b155de316738e794dc7606a8
zach2heth <zach2heth@gmail.com>
2017-10-10 20:36:13 -1000

    Task 5: Security with methods, Filtering data with publish and subscribe- THE REAL ONE

--- app/server/main.js
+++ app/server/main.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 import { Tasks} from '../imports/api/tasks';
 
+import { Meteor } from 'meteor/meteor'
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
